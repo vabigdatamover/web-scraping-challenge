b6be5063be41a58731a344ff547b03b7810e9545
vabigdatamover <vabigdatamover@gmail.com>
2019-08-27 23:11:07 -0400

    Web Scrape Homework

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, redirect, request
 from flask_pymongo import PyMongo
-import web_scrape
+import mission_to_mars
 
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 # Or set inline
