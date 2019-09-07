2829dc0610d061a1707b6cea58796a4bb0a3ae9b
vabigdatamover <vabigdatamover@gmail.com>
2019-09-07 14:37:33 -0400

    Web Scrape Homework

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created 2019
-@author: GB
+Author: GB
 """
 
 from flask import Flask, render_template, redirect
@@ -17,11 +17,9 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
-def home():
-
+def index():
     # Find one record of data from the mongo database
     mars_data = mongo.db.collection.find_one()
-
     # Return template and data
     return render_template("index.html", mars_dict=mars_data)
 
@@ -29,15 +27,22 @@ def home():
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
+    mars=mongo.db.mars_data
+    # Run the scrape function
+    #mars_data=scrape_mars.scrape_all()
+    mars_data=mars.scrape_all()
+    # Update the Mongo database using update and upsert=True
+    mars.update({}, mars_data, upsert=True)
+    return "Scraping Successful"
 
     # Run the scrape function
-    scrape_mars_data = scrape_mars.scrape()
+    #scrape_mars_data = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, scrape_mars_data, upsert=True)
+    #mongo.db.collection.update({}, scrape_mars_data, upsert=True)
 
     # Redirect back to home page
-    return redirect("/")
+    #return redirect("/")
 
 
 if __name__ == "__main__":
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on 2019@
-@author: GB
+Created on 2019
+Author: GB
 """
 
 from splinter import Browser
@@ -98,39 +98,7 @@ def scrape():
     """
     Mars Hemispheres
     """
-    url4 = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-    browser.visit(url4)
-    time.sleep(1)
-
-    html = browser.html
-    soup = BeautifulSoup(html, 'html.parser')
-    
-    hemisphere_image_urls = []
-    
-    hemis_titles = soup.find_all('h3')
-    
-    for i in range(len(hemis_titles)):
-        
-        hemis_title = hemis_titles[i].text
-        
-        hemis_images = browser.find_by_tag('h3')
-        hemis_images[i].click()
-        time.sleep(1)
-
-        html = browser.html
-        soup = BeautifulSoup(html, 'html.parser')
-        
-        img_url = soup.find('img', class_='wide-image')['src']
-        img_url = "https://astrogeology.usgs.gov" + img_url
-        
-        hemis_dict = {"title": hemis_title, "img_url":img_url}
-        hemisphere_image_urls.append(hemis_dict)
-                        
-        browser.back()
-    
-    mars_hemispheres_dict = {"mars_hemispheres_dict":hemisphere_image_urls}
-    
-    return_dict.update(mars_hemispheres_dict)
+  
 
     browser.quit()
     
