e7bcbcbc36c738f32ce36828eede182e5d2b24c6
vabigdatamover <vabigdatamover@gmail.com>
2019-09-07 22:59:27 -0400

    Web Scrape Homework

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -12,28 +12,29 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_data")
 
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
-def index():
+def home():
     # Find one record of data from the mongo database
-    mars_data = mongo.db.collection.find_one()
+    mission_data = mongo.db.collection.find_one()
     # Return template and data
-    return render_template("index.html", mars_dict=mars_data)
+    return render_template("index.html", mars_dict=mission_data)
 
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
-    mars=mongo.db.mars_data
+    #mars=mongo.db.mars_data
     # Run the scrape function
-    #mars_data=scrape_mars.scrape_all()
-    mars_data=mars.scrape_all()
+    mars_data=scrape_mars.scrape_info()
     # Update the Mongo database using update and upsert=True
-    mars.update({}, mars_data, upsert=True)
-    return "Scraping Successful"
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars_data, upsert=True)
+    #mars.update({}, mars_data, upsert=True)
+    #return "Scraping Successful"
 
     # Run the scrape function
     #scrape_mars_data = scrape_mars.scrape()
@@ -42,8 +43,8 @@ def scrape():
     #mongo.db.collection.update({}, scrape_mars_data, upsert=True)
 
     # Redirect back to home page
-    #return redirect("/")
+    return redirect("/")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -8,22 +8,27 @@ from splinter import Browser
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
-import pymongo
+#import pymongo
 
 # Setup connection to mongodb
-conn = "mongodb://localhost:27017"
-client = pymongo.MongoClient(conn)
+#conn = "mongodb://localhost:27017"
+#client = pymongo.MongoClient(conn)
 
 # Select database and collection to use
-db = client.mars_data
-collection = db.mars_data
+#db = client.mars_data
+#collection = db.mars_data
 
-def scrape():
+def init_browser():
+   # @NOTE: Path to my chromedriver
+   executable_path = {"executable_path": 'chromedriver.exe'}
+   return Browser("chrome", **executable_path, headless=False)
+
+def scrape_info():
+    browser=init_browser()
+    #return_dict = {}
     
-    return_dict = {}
-    
-    executable_path = {'executable_path': 'chromedriver.exe'}
-    browser = Browser('chrome', **executable_path, headless=False)
+    #executable_path = {'executable_path': 'chromedriver.exe'}
+    #browser = Browser('chrome', **executable_path, headless=False)
     
     """
     NASA Mars News
@@ -36,12 +41,10 @@ def scrape():
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
 
-    news_title =  soup.find('div', class_='content_title').a.text
+    news_title =  soup.find('div', class_='content_title').text
     news_p = soup.find('div', class_='article_teaser_body').text
 
-    mars_news_dict = {"news_title":news_title, "news_p":news_p}
     
-    return_dict.update(mars_news_dict)
     
     """
     JPL Mars Space Images - Featured Image
@@ -59,9 +62,7 @@ def scrape():
     split_text = style.split("'")
     featured_image_url = 'https://www.jpl.nasa.gov' + split_text[1]
     
-    featured_image_dict = {"featured_image_url":featured_image_url}
     
-    return_dict.update(featured_image_dict)
     
     """
     Mars Weather
@@ -76,9 +77,7 @@ def scrape():
     tweet_text_container = soup.find('div', class_='js-tweet-text-container')
     mars_weather = tweet_text_container.p.text 
     
-    mars_weather_dict = {"mars_weather":mars_weather}
-    
-    return_dict.update(mars_weather_dict)
+   
     
     """
     Mars Fact
@@ -93,13 +92,22 @@ def scrape():
     df.set_index('description', inplace=True)
     
     mars_fact_dict = {"mars_fact":df.to_html()}
-    return_dict.update(mars_fact_dict)
+    mars_dict.update(mars_fact_dict)
     
     """
     Mars Hemispheres
     """
-  
+  # Store data in a dictionary
+    mars_data = {
+       "news_title": news_title,
+       "news_p": news_p,
+       "featured_image_url": featured_image_url,
+       #"featured_img_title": featured_img_title,
+       "mars_weather": mars_weather,
+       "html_table":html_table,
+       #"hemisphere_image_urls":hemisphere_image_urls
+   }
 
     browser.quit()
     
-    return return_dict
+    return mars_data
